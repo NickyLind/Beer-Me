41d29d0b6fff2e1c50d3087edb8a753a1aa5e426
Nick Lindau <nicholaithegreat@gmail.com>
2021-05-19 16:34:38 -0700

    sidebar toggle now blurs background components

--- src/components/mainScreen/MainScreenController.js
+++ src/components/mainScreen/MainScreenController.js
@@ -4,28 +4,47 @@ import classes from "./MainScreenController.module.css";
 import FormController from "./forms/FormController";
 import LogInController from "./login/LogInController";
 import SidebarController from "../SidebarController";
+import { connect } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function MainScreenController() {
+function MainScreenController(props) {
+
+  const fadeHome = {
+    filter: "blur(2px)",
+    webkitFilter: "blur(2px)"
+  }
+  const background = {
+    backgroundColor: "blue"
+  }
 
   return (
     <Router>
       <SidebarController />
       <Switch>
-        <Route path="/login">
-          <LogInController />
-        </Route>
-        <Route path="/userBreweries">
-          <FormController />
-        </Route>
-        <Route path="/">
-          <div className={classes.background}>
-            <Home />
-          </div>
-        </Route>
+        <div style={props.sidebarVisible ? fadeHome : background}>
+          <Route path="/login">
+            <LogInController />
+          </Route>
+          <Route path="/userBreweries">
+            <FormController />
+          </Route>
+          <Route path="/">
+            <div className={classes.background}>
+              <Home />
+            </div>
+          </Route>
+        </div>
       </Switch>
     </Router>
   );
 }
+
+const mapStateToProps = state => {
+  return {
+    sidebarVisible: state.sidebarVisible
+  }
+};
+
+MainScreenController = connect(mapStateToProps)(MainScreenController);
 
 export default MainScreenController;
